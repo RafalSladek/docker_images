3e8133354b14d74bee2208bbdc60dce9cb0d86ef
Rafal <rafal.sladek@gmail.com>
2017-10-18 22:45:53 +0200

    added build and binary for xmrig on ubuntu 16.04

---
+++ build-xmrig-ubuntu-16.04.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -uex
+
+osVersion="16.04"
+os="ubuntu"
+miner="xmrig"
+image_name=$miner-$os-$osVersion
+dockerfile=Dockerfile-$miner-$os-$osVersion
+
+if [ -d $miner ]; then
+  git -C $miner clean -fdx
+else
+  git clone https://github.com/rafalsladek/$miner.git $miner
+fi
+
+docker build -t $image_name -f $dockerfile .
+
+docker run --rm -it -v $PWD/$miner:/$miner $image_name:latest /bin/bash -c "
+set -ex ;
+cd /$miner ;
+mkdir bin
+cd bin
+cmake .. -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7 -DWITH_LIBCPUID=ON -DWITH_AEON=ON -DWITH_HTTPD=ON;
+make  ;
+"
+
+mv $miner/bin/$miner bin/$image_name
+mv $miner/src/config.json bin/config.json
+git -C $miner clean -fdx
